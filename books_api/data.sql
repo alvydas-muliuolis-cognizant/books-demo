e64e174bc07b659212b023e4986d2cb107d0eb6a
Alvydas Muliuolis <alvydas.muliuolis@cognizant.com>
2025-01-30 18:09:58 +0200

    add author and publiser tables

--- books_api/data.sql
+++ books_api/data.sql
@@ -1,4 +1,41 @@
-INSERT INTO book (title) 
-VALUES
-	('Python Crash Course'),
-	('Automate the Boring Stuff with Python');
+begin transaction;
+insert into
+	publisher ("name")
+values   
+	('No Starch Press'),
+	('O''Reilly Media'),
+	('The MIT Press');
+
+insert into 
+	author ("full_name")
+values
+	('Eric Matthes'),
+	('Al Sweigart'),
+	('Luciano Ramalho'),
+	('Wes McKinney'),
+	('Thomas H. Cormen'),
+	('Charles E. Leiserson'),
+	('Ronald L. Rivest'),
+	('Clifford Stein');
+
+insert into
+	book ("title", "isbn13", "pages", "overview", "publisher_id")
+values
+	('Python Crash Course','9781718502703',552,'Python Crash Course is the world’s best-selling guide to the Python programming language. This fast-paced, thorough introduction will have you writing programs, solving problems, and developing functioning applications in no time.',1),
+	('Automate the Boring Stuff with Python','9781593279929',592,'Learn how to code while you write programs that effortlessly perform useful feats of automation!',1),
+	('Fluent Python','9781492056355',1012,'Don''t waste time bending Python to fit patterns you''ve learned in other languages. Python''s simplicity lets you become productive quickly, but often this means you aren''t using everything the language has to offer. With the updated edition of this hands-on guide, you''ll learn how to write effective, modern Python 3 code by leveraging its best ideas.',2),
+	('Python for Data Analysis','9781098104030',579,'Get the definitive handbook for manipulating, processing, cleaning, and crunching datasets in Python. Updated for Python 3.10 and pandas 1.4, the third edition of this hands-on guide is packed with practical case studies that show you how to solve a broad set of data analysis problems effectively. You''ll learn the latest versions of pandas, NumPy, and Jupyter in the process.',2),
+	('Introduction to Algorithms','9780262046305',1312,'A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.',3);
+
+insert into
+	book_author ("book_id", "author_id")
+values
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5),
+	(5, 6),
+	(5, 7),
+	(5, 8);
+commit transaction;
--- books_api/src/models.py
+++ books_api/src/models.py
@@ -1,8 +1,14 @@
-from sqlalchemy import String
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    String,
+    Table,
+)
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     mapped_column,
+    relationship,
 )
 
 
@@ -10,8 +16,44 @@ class Base(DeclarativeBase):
     pass
 
 
+book_author = Table(
+    "book_author",
+    Base.metadata,
+    Column("book_id", ForeignKey("book.id"), primary_key=True),
+    Column("author_id", ForeignKey("author.id"), primary_key=True),
+)
+
+
 class Book(Base):
     __tablename__ = "book"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
+    isbn13: Mapped[str] = mapped_column(String(13), nullable=False)
+    pages: Mapped[int]
+    overview: Mapped[str] = mapped_column(String(1000), nullable=False)
+    publisher_id: Mapped[int] = mapped_column(ForeignKey("publisher.id"))
+    authors: Mapped[list["Author"]] = relationship(
+        secondary=book_author,
+        back_populates="books",
+    )
+    publisher: Mapped["Publisher"] = relationship(back_populates="books")
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    books: Mapped[list["Book"]] = relationship(
+        secondary=book_author,
+        back_populates="authors",
+    )
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    books: Mapped[list["Book"]] = relationship(back_populates="publisher")
